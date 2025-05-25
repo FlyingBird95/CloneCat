3c7c26ae44064b43207793ceaf0ad14075fc6e81
Patrick <patrick@paylogic.eu>
2025-05-25 14:41:06 +0200

    Renamed to Twinly

--- src/twin/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Twin - Create perfect twins of your Python objects.
-
-A library for cloning Python objects with their relationships intact.
-"""
-
-__version__ = "0.1.1"
-__author__ = "Patrick"
