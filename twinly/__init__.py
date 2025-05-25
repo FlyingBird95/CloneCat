1b7fb3c597132ff33698ffb2967cb0b76edca763
GitHub Action <action@github.com>
2025-05-25 13:41:37 +0000

    Bump version: 0.1.10 → 0.1.11

--- twinly/__init__.py
+++ twinly/__init__.py
@@ -5,5 +5,5 @@ This is a Python library
 that creates perfect copies of your objects along with all their relationships.
 """
 
-__version__ = "0.1.10"
+__version__ = "0.1.11"
 __author__ = "Patrick"
